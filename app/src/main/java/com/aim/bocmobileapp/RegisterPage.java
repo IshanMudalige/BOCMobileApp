3d97bc9a18563bfece378091e9cb12671230e825
sampathw98 <sampathchanaka.bandara@gmail.com>
2020-10-05 23:39:49 +0530

    Log In And Register Interface

---
+++ app/src/main/java/com/aim/bocmobileapp/RegisterPage.java
@@ -0,0 +1,15 @@
+package com.aim.bocmobileapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterPage extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_page);
+    }
+}
