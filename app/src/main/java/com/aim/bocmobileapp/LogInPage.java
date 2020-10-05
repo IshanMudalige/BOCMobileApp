5b1f43adb55bad0cf56b3d73527e385da14b4bcd
sampathw98 <sampathchanaka.bandara@gmail.com>
2020-10-05 23:40:45 +0530

    Log In And Register Interface

---
+++ app/src/main/java/com/aim/bocmobileapp/LogInPage.java
@@ -0,0 +1,51 @@
+package com.aim.bocmobileapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+public class LogInPage extends AppCompatActivity {
+
+    Button login;
+    EditText username,pwd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in_page);
+
+        login = (Button)findViewById(R.id.btnLoginUser);
+        username = (EditText)findViewById(R.id.etUserNameLogin);
+        pwd = (EditText)findViewById(R.id.etPasswordLogin);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivityLogInUser();
+            }
+        });
+    }
+
+    private void openActivityLogInUser(){
+        String user = username.getText().toString().trim();
+        String password = pwd.getText().toString().trim();
+
+        if(user.equals("Kamal") && password.equals("Kamal123")){
+            Toast.makeText(this,"LOG IN SUCCESSFUL",Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(this, ForgotPassword.class);
+            startActivity(intent);
+        } else {
+            Toast.makeText(this,"LOG IN FAILED",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+}
